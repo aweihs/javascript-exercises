7cf9d39c87fbac59de31a4cf222824793767aa3d
Ant Weihs <54947317+aweihs@users.noreply.github.com>
2024-02-23 14:54:04 -0500

    Complete JS exercise 3
    JS exercise 3 completed and functional with all tests passing.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,25 @@
-const reverseString = function() {
+const reverseString = function(input) {
+
+    let stringNorm = input;
+    let reverseStringVal = input.split('');
+    let arrStringNorm = input.split('');
+
+    if(input === '') {
+
+        return '';
+
+    } else {
+
+        for(i=0; i < stringNorm.length + 1; i++) {
+
+            reverseStringVal.push(arrStringNorm[arrStringNorm.length - i]);
+            reverseStringVal.shift();
+
+        }
+
+        return reverseStringVal.join('');
+
+    }
 
 };
 
