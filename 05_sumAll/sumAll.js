9a01b63e5cec4c98a2197bd084ec3c3f7490ab5f
Ant Weihs <54947317+aweihs@users.noreply.github.com>
2024-02-27 10:47:16 -0500

    Complete JS exercise 5
    JS exercise 5 complete and functional, creating a sum for every number in between two integer parameters.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,32 @@
-const sumAll = function() {
+const sumAll = function(valOne, valTwo) {
 
+    let sum = 0;
+
+    if (valOne < 0 || valTwo < 0) return 'ERROR';
+    if (!Number.isInteger(valOne) || !Number.isInteger(valTwo)) return 'ERROR';
+
+    if(valOne > valTwo) {
+
+        for(i = valTwo; i < valOne + 1; i++) {
+            sum += i;
+        }
+
+        return sum;
+
+    } else if(valOne < valTwo) {
+
+        for(i = valOne; i < valTwo + 1; i++) {
+            sum += i;
+        }
+
+        return sum;
+
+    } else {
+
+        sum = valOne + valTwo;
+        return sum;
+
+    }
 };
 
 // Do not edit below this line
